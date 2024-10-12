ff4125a03ebdd80fe99f60856e738fdb25428609
162519 <125856974+162519@users.noreply.github.com>
2024-10-12 16:14:35 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    //用于内存消耗测试，读入起始点地址
+    
     // if (argc != 8) {
     //     fprintf(stderr, "Usage: %s <RDataset> <PDataset> <RVertex Number> <PVertex Number> <Output Dir> <ThreadNum> <startsIdsfile>\n", argv[0]);   
     //     return -1;
@@ -22,8 +22,8 @@ int main(int argc, char* argv[]) {
     unsigned int P_vnums = atoi(argv[4]);
     std::string Output_dir = argv[5];//sky211116
     int ThreadNum = atoi(argv[6]);
-    // string startsIdsfile = argv[7]; //用于内存消耗测试，读入起始点地址
-    //std::string flag=argv[6];//模式图的最小匹配节点是否有等价（对称）点，若有flag为true，否则flag为false
+    // string startsIdsfile = argv[7];
+    //std::string flag=argv[6];
   cout<<"==============================================================================="<<endl;
 
   // cout<<R_input_file<<endl;
@@ -31,9 +31,9 @@ int main(int argc, char* argv[]) {
 
   // R_input_file = "../../dataset/Orkut.txt";
 	PatternMatching* pm = new PatternMatching();
-  // pm->startsIdsfilename = startsIdsfile; //用于内存消耗测试，读入起始点地址
+  // pm->startsIdsfilename = startsIdsfile; 
   struct timeval load_start_time, load_end_time, load_tmp_start, load_tmp_end;
-  gettimeofday(&load_start_time, NULL);//起始时间
+  gettimeofday(&load_start_time, NULL);
   // pm->build_degree_Rs(R_input_file, R_vnums);
 	// pm->build_R_adjs(R_input_file);
   pm->build_degree_R(R_input_file, R_vnums);
@@ -44,7 +44,7 @@ int main(int argc, char* argv[]) {
   (load_tmp_end.tv_usec - load_start_time.tv_usec)) / 1000000.0 << " s" << std::endl;
 
   struct timeval pre_start_time, pre_end_time, pre_tmp_start, pre_tmp_end;
-  gettimeofday(&pre_start_time, NULL);//起始时间
+  gettimeofday(&pre_start_time, NULL);
 	pm->init(Output_dir, ThreadNum);//sky211116
   pm->matchPR_expand();
   pm->findSym();
@@ -59,9 +59,9 @@ int main(int argc, char* argv[]) {
   cout<<R_input_file<<endl;
   cout<<P_input_file<<endl;
 
-  // 打印挖掘开始时间
+  
   // time_t now = time(nullptr);  
-  // tm* curr_tm = gmtime(&now);  // 返回的结构体存储位置未知，不知何时释放，因此推荐使用安全版本。  
+  // tm* curr_tm = gmtime(&now);   
   // char time[80] = {0};  
   // strftime(time, 80, "%Y-%m-%d %H:%M:%S", curr_tm);  
   // cout<< "Start mining time: "<<time<<endl;
@@ -71,7 +71,7 @@ int main(int argc, char* argv[]) {
 
 
   struct timeval start_time, end_time, tmp_start, tmp_end;
-  gettimeofday(&start_time, NULL);//起始时间
+  gettimeofday(&start_time, NULL);
   // pm->isEqCircle = false;
   if(pm->isEqCircle){
     pm->searchALLCircle();
